be58f680ac72becc0805154906bb736fc2f74491
Hamed Rezaie Saniani <Hamed.r.Saniani@outlook.com>
2021-11-22 22:07:12 +0330

    Implement Main Page & Infrastructure.Query Project

--- MB.Infrastructure.Core/Bootsrapper.cs
+++ MB.Infrastructure.Core/Bootsrapper.cs
@@ -7,6 +7,7 @@ using MB.Domain.ArticleCategoryAgg;
 using MB.Domain.ArticleCategoryAgg.Services;
 using MB.Infrastructure.EFCore;
 using MB.Infrastructure.EFCore.Repository;
+using MB.Infrastructure.Query;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,6 +23,7 @@ namespace MB.Infrastructure.Core
             services.AddTransient<IArticleRepository, ArticleRepository>();
             services.AddTransient<IArticleCategoryValidatorService, ArticleCategoryValidatorService>();
             services.AddTransient<IArticleValidatorService, ArticleValidatorService>();
+            services.AddTransient<IArticleQuery, ArticleQuery>();
             services.AddDbContext<MasterBloggerContext>(options => {
                 options.UseSqlServer(connectionString);
             });
---
+++ MB.Infrastructure.Query/ArticleQuery.cs
@@ -0,0 +1,31 @@
+﻿using MB.Infrastructure.EFCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace MB.Infrastructure.Query
+{
+    public class ArticleQuery : IArticleQuery
+    {
+        private readonly MasterBloggerContext _context;
+
+        public ArticleQuery(MasterBloggerContext context)
+        {
+            _context = context;
+        }
+
+        public List<ArticleQueryViewModel> GetArticles()
+        {
+            return _context.Articles.Include(x => x.ArticleCategory).Select(x => new ArticleQueryViewModel
+            {
+                Id = x.Id,
+                Title = x.Title,
+                CreationDate = x.CreationDate.ToString(CultureInfo.InvariantCulture),
+                ArticleCategory = x.ArticleCategory.Title,
+                Image = x.Image,
+                ShortDescription = x.ShortDescription
+            }).ToList();
+        }
+    }
+}
